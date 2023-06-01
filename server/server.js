16e62d8cfa0a539b18601ef41ad85f1d6fe62f91
Ayush0202 <ayush.skg@gmail.com>
2023-06-01 19:18:51 +0530

    db connected

--- server/server.js
+++ server/server.js
@@ -1,6 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
 const app = express()
+
+dotenv.config()
+
+app.use(bodyParser.json({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors())
+
+// connecting database
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.cx5ccid.mongodb.net/${process.env.DB_NAME}`, {
+    useUnifiedTopology: true })
+    .then(() => console.log('Database Connected'))
+    .catch((err) => console.log(err))
 
 app.get('/', (req, res) => {
     res.send('Hello from Server')
