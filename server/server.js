7147c4ed1f9b5cc922b54eb93fdef8a74712e899
Ayush0202 <ayush.skg@gmail.com>
2023-06-16 19:59:16 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -17,7 +17,10 @@ const authRoute = require('./routes/authRoute')
 
 app.use(bodyParser.json({extended: true}))
 app.use(bodyParser.urlencoded({extended: true}))
-app.use(cors())
+app.use(cors({
+    origin: 'https://wordsync-one.vercel.app',
+    optionsSuccessStatus: 200 
+}));
 
 // connecting database
 mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.cx5ccid.mongodb.net/${process.env.DB_NAME}`, {
